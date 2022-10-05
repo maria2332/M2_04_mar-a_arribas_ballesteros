d36a118a6546d34445ef697fe7d6c0b8b94f3aea
maria2332 <113980788+maria2332@users.noreply.github.com>
2022-10-05 12:48:46 +0000

    ejercicio1

---
+++ ejercicio1.py
@@ -0,0 +1,17 @@
+#ejercicio 1
+tupla = ("pala","pelota","camiseta","pantalon")
+print(tupla)
+
+lista = list(tupla)
+
+lista.append("mano")
+print(lista)
+
+print (lista [1])
+print(len(lista))
+
+lista[1:2]=["carro"]
+print(lista)
+
+lista.clear()
+print (lista)
