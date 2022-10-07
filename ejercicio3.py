d8e2e19b781cbec1230d54b645032006b80a48a1
maria2332 <113980788+maria2332@users.noreply.github.com>
2022-10-07 09:25:17 +0000

    ejercicios 3 y 4

--- ejercicio3.py
+++ ejercicio3.py
@@ -4,10 +4,10 @@ num2 = float(input("Introduzca otro numero: "))
 num3 = float(input("Introduzca otro numero: "))
 
 lista=(num1,num2,num3)
-print(sum(lista))
+print("El sumatorio total de la lista es: ",sum(lista))
 sumatorio = num1 + num2 +num3
 
 #ejercicio 4
 t_numeros = len(lista)
 media = (sumatorio)/t_numeros
-print(media)
+print("La media es: ",media)
